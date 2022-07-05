0ae41751f80ecfb2d157703d28454c1067c372a4
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-05 21:01:56 +0100

    create new route and add api

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+players = [
+    {'id': 1, 'name': 'Lebron James', 'age': 37},
+    {'id': 2, 'name': 'Kevin Durant', 'age': 32},
+    {'id': 3, 'name': 'Kyrie Irving', 'age': 30}
+]
+
 @app.route('/')
 def hello():
+    return f"Hello world"
+
+@app.route('/players')
+def index():
     return f"Hello world"
 
 if __name__ == "__main__": 
